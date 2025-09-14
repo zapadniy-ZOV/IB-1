7b7770c489879b24bba2c6f9095b1e09c838fb13
ta4ilka69 <artem_balin_2004@mail.ru>
2025-09-14 22:43:05 +0300

    simple project init

---
+++ secure-api/src/main/java/itmo/labs/secure_api/SecureApiApplication.java
@@ -0,0 +1,13 @@
+package itmo.labs.secure_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApiApplication.class, args);
+	}
+
+}
